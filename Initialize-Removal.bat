0553f4d017f4a3ebd9cbe4cd0e28ff4390b2ec91
onashia <me@onashia.com>
2023-12-15 17:51:21 -0800

    feat: download AdwCleaner and Malwarebytes Toolkit

---
+++ Initialize-Removal.bat
@@ -0,0 +1,32 @@
+@echo off
+
+:: Description     > Download and run AdwCleaner and Malwarebytes Toolkit within BeyondTrust Bomgar
+
+:: General Variables
+set "key=INSERT_KEY_HERE"
+set "folder=C:\STS\Tools\Malwarebytes"
+
+:: AdwCleaner Variables
+set "adwApp=adwcleaner.exe"
+set "adwFile=adwcleaner.exe"
+set "adwUrl=https://adwcleaner.malwarebytes.com/adwcleaner?channel=release"
+
+:: Malwarebytes Variables
+set "mwbApp=MBTSLauncher.exe"
+set "mwbFile=MBTS.zip"
+set "mwbUrl=https://toolset.malwarebytes.com/file/mbts/%key%"
+
+:: Create %folder%
+if not exist %folder% ( mkdir %folder% && echo %folder% created)
+
+:: Download AdwCleaner
+echo 1. Downloading AdwCleaner
+PowerShell -NoProfile -Command "(New-Object Net.WebClient).DownloadFile('%adwUrl%', '%folder%\%adwFile%')"
+
+:: Open and initiate an AdwCleaner Scan
+echo 2. Starting AdwCleaner
+start %folder%\%adwApp% /noreboot /clean
+
+:: Download Malwarebytes
+echo 3. Downloading Malwarebytes
+PowerShell -NoProfile -Command "(New-Object Net.WebClient).DownloadFile('%mwbUrl%', '%folder%\%mwbFile%')"
