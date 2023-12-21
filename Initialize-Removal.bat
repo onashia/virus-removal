db96cf06d5c67614f223e425eaa5b65846148ec9
onashia <me@onashia.com>
2023-12-20 16:00:41 -0800

    feat: perform disk cleanup

--- Initialize-Removal.bat
+++ Initialize-Removal.bat
@@ -20,26 +20,34 @@ set "mwbSettings=INSERT_SETTINGS_LINK_HERE"
 :: Create %folder%
 if not exist %folder% ( mkdir %folder% && echo %folder% created)
 
+:: Run disk cleanup with all options checked
+echo 1. Performing disk cleanup
+cleanmgr /d c: /verylowdisk
+
 :: Download AdwCleaner
-echo 1. Downloading AdwCleaner
+echo 2. Downloading AdwCleaner
 PowerShell -NoProfile -Command "(New-Object Net.WebClient).DownloadFile('%adwUrl%', '%folder%\%adwFile%')"
 
 :: Open and initiate an AdwCleaner scan
-echo 2. Starting AdwCleaner
+echo 3. Starting AdwCleaner
 start %folder%\%adwApp% /eula /noreboot /clean
 
 :: Download Malwarebytes
-echo 3. Downloading Malwarebytes
+echo 4. Downloading Malwarebytes
 PowerShell -NoProfile -Command "(New-Object Net.WebClient).DownloadFile('%mwbUrl%', '%folder%\%mwbFile%')"
 
 :: Extract the compressed Malwarebytes file
-echo 4. Extracting Malwarebytes
+echo 5. Extracting Malwarebytes
 PowerShell -Command "Expand-Archive -LiteralPath '%folder%\%mwbFile%' -DestinationPath '%folder%'" >nul
 
-echo 5. Applying Malwarebytes settings
+echo 6. Applying Malwarebytes settings
 PowerShell -NoProfile -Command "(New-Object Net.WebClient).DownloadFile('%mwbSettings%', '%folder%\Malwarebytes\MBTS\Data\Configuration\mbts-settings.json')"
 
 :: Open an initiate a Malwarebytes scan
-echo 6. Starting Malwarebytes
+echo 7. Starting Malwarebytes
 start %folder%\%MwbApp% /scan:issues
 start %folder%\%MwbApp% /scan:malware
+
+:: Open Windows Update window for updates
+echo 8. Opening Windows Update
+start ms-settings:windowsupdate
